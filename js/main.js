6849b3b5a49e853ed301de7702a49566dab56091
Eric Serafim <ericserafim@gmail.com>
2018-10-15 17:22:49 +1300

    Ajustes finais do projeto

--- js/main.js
+++ js/main.js
@@ -6,15 +6,23 @@ $(document).ready(function () {
 
     $(document).on('click', 'a[href^="#"]', function (event) {
         event.preventDefault();
-    
+
         $('html, body').animate({
             scrollTop: $($.attr(this, 'href')).offset().top
         }, 500);
 
-        $('header .navbar-nav a').on( 'click', function () {
+        $('header .navbar-nav a').on('click', function () {
             $('header .navbar-nav').find('li.active').removeClass('active');
-            $(this ).parent('li').addClass('active');
+            $(this).parent('li').addClass('active');
         });
+    });
+
+    $(function () {
+        $('[data-toggle="popover"]').popover()
+    });
+
+    $('.popover-dismiss').popover({
+        trigger: 'focus'
     });
 });
 
